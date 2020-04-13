3229f44028da5a8422968ac6e3a36b8dd40f140d
strk2333 <strk23@outlook.com>
2020-04-13 08:09:08 +0800

    insect not finished

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -18,3 +18,26 @@ export const FISH_SIZE = {
     get TINY() { return { id: 5, name: 'sea', cn: '细长' }; },
     get FIN() { return { id: 5, name: 'sea', cn: '背鳍' }; },
 };
+
+export const INSECT_LOC = {
+    get RIVER() { return { id: 1, name: 'river', cn: '草地,地面跳跃' }; },
+    get POOL() { return { id: 2, name: 'pool', cn: '绿地异色花,飞行' }; },
+    get CLIFF() { return { id: 3, name: 'cliff', cn: '绿地,飞行' }; },
+    get ESTUARY() { return { id: 4, name: 'estuary', cn: '花朵' }; },
+    get SEA() { return { id: 5, name: 'sea', cn: '树上拟态' }; },
+    get WHARF() { return { id: 6, name: 'wharf', cn: '树桩' }; },
+    get RANDOM() { return { id: 7, name: 'random', cn: '树干' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '树干,除椰子树香蕉树以外' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '树干,摇晃或敲击' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '沙滩,贝壳' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '水边,飞行' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '池塘,水面滑行' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '河流池塘' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '飞行,腐烂的大头菜和垃圾等' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+    // get SEA_RAIN() { return { id: 5, name: 'sea', cn: '大海,雨天' }; },
+};
